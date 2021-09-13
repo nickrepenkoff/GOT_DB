8d5825cf0025a8f375587340c49c6127642a944a
Nickrepenkoff <nikita.repenko@gmail.com>
2021-09-13 21:16:44 +0300

    Updates

---
+++ precache-manifest.5608450f3dc8061d3c2a23a337c006c3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "46e9ea33c78740ae8cf1",
+    "url": "./static/css/main.a05393c9.chunk.css"
+  },
+  {
+    "revision": "46e9ea33c78740ae8cf1",
+    "url": "./static/js/main.46e9ea33.chunk.js"
+  },
+  {
+    "revision": "f4bdf04d6144dc261f32",
+    "url": "./static/css/1.41195eef.chunk.css"
+  },
+  {
+    "revision": "f4bdf04d6144dc261f32",
+    "url": "./static/js/1.f4bdf04d.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "22389cf47ad284464c13506f2e22e8af",
+    "url": "./static/media/error.22389cf4.jpg"
+  },
+  {
+    "revision": "4da810837c6dd45262462cccf9566c34",
+    "url": "./static/media/got.4da81083.jpeg"
+  },
+  {
+    "revision": "e8e857ac60adee5958e1696565336a53",
+    "url": "./index.html"
+  }
+];
