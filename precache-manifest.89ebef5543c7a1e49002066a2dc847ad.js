6c9e1162727536eb0abc3d01698499b0f3787e73
Nickrepenkoff <nikita.repenko@gmail.com>
2021-09-13 18:16:25 +0300

    Updates

---
+++ precache-manifest.89ebef5543c7a1e49002066a2dc847ad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe144c59a31b5da1f7fe",
+    "url": "/nickrepenkoff/GOT_DB/static/css/main.4298b304.chunk.css"
+  },
+  {
+    "revision": "fe144c59a31b5da1f7fe",
+    "url": "/nickrepenkoff/GOT_DB/static/js/main.fe144c59.chunk.js"
+  },
+  {
+    "revision": "f4bdf04d6144dc261f32",
+    "url": "/nickrepenkoff/GOT_DB/static/css/1.41195eef.chunk.css"
+  },
+  {
+    "revision": "f4bdf04d6144dc261f32",
+    "url": "/nickrepenkoff/GOT_DB/static/js/1.f4bdf04d.chunk.js"
+  },
+  {
+    "revision": "25674550c326661635f9",
+    "url": "/nickrepenkoff/GOT_DB/static/js/runtime~main.25674550.js"
+  },
+  {
+    "revision": "22389cf47ad284464c13506f2e22e8af",
+    "url": "/nickrepenkoff/GOT_DB/static/media/error.22389cf4.jpg"
+  },
+  {
+    "revision": "4da810837c6dd45262462cccf9566c34",
+    "url": "/nickrepenkoff/GOT_DB/static/media/got.4da81083.jpeg"
+  },
+  {
+    "revision": "4e1fc504caff054e3465644c4bd9c811",
+    "url": "/nickrepenkoff/GOT_DB/index.html"
+  }
+];
